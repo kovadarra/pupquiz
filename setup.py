5ad0e3cb71824d02b3fa1c9b087312617a9f8942
kovadarra <58000806+kovadarra@users.noreply.github.com>
2020-06-25 20:41:47 +0300

    Fix #10000 to 'main thread not in main loop'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="pupquiz",
-    version="0.0.17",
+    version="0.0.18",
     author="kovadarra",
     author_email="kovadarra@users.noreply.github.com",
     description="Facilitates vocabulary acquisition",
